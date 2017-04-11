9015d18e2fc1d5e45407b05209b9d9bcf7c27cd8
Agata Rojek <agata85rojek@gmail.com>
2017-04-12 01:41:12 +0200

    The form that sends and displays the name

--- zadanie1/index.php
+++ zadanie1/index.php
@@ -1,2 +1,31 @@
 <?php
 
+if ($_SERVER['REQUEST_METHOD'] === 'POST'){
+    
+    if(isset($_POST['name'])){
+        
+        $name = $_POST['name'];
+    }
+    
+}
+
+?>
+
+<!doctype_html>
+<html>
+    <body>
+        <h2>
+            <?php
+            if(isset($name)){
+                echo '&quot;Hello, '.$name.'!&quot;';
+            }
+            
+            ?>
+        </h2>
+        <form action="" method="post">
+            <input type="text" name="name" placeholder="Name">
+            <button type="submit">Send</button>
+        </form>
+    </body>
+</html>
+
